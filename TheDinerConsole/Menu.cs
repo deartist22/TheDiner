e7992f3207575c57a3f0ec40634d5e4fcee5fa20
deartist22 <deartist22@gmail.com>
2019-10-15 17:19:35 -0500

    Add project files.

---
+++ TheDinerConsole/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheDinerConsole
+{
+    public class Menu
+    {
+        public string Name;
+        public List<MenuItem> items;
+
+        public Menu()
+        {
+            items = new List<MenuItem>();
+        }
+
+    }
+}
---
+++ TheDinerConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TheDinerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu summerMenu = new Menu();
+            summerMenu.Name = "Summer Menu";
+            MenuItem salmon = new MenuItem();
+            salmon.Title = "Salmon";
+            salmon.Description = "Fresh Norwegian Salmon";
+            salmon.Price = 25.50;
+            summerMenu.items.Add(salmon);
+            Console.WriteLine("Done");
+        }
+    }
+}
